82f59a009aecacf93aeb36b97dfcc34aa14241c8
AlvaGoncalves <alva_27goncalves@hotmail.com>
2026-08-24 15:36:22 -0300

    FIX: errores por carmbio de version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
@@ -21,27 +20,6 @@ export default defineConfig({
         locales: {
           en: "en",
         },
-      },
-    }),
-    starlight({
-      title: "Sinergia Valores S.A",
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "Español",
-          lang: "es",
-        },
-        es: {
-          label: "Español",
-          lang: "es",
-        },
-      },
-      disable404Route: true,
-      customCss: ["./src/styles/global.css"],
-      favicon: "/favicon.ico",
-      components: {
-        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-        Head: "./src/components/ui/starlight/Head.astro",
       },
     }),
     compressor({
